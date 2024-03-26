8adad721f259a0dbbe967d5f006e57ac93d24d61
Bakary Sylla <syllabakary2002@gmail.com>
2024-03-26 03:55:54 -0700

    Event listener updated with error catch

--- script.js
+++ script.js
@@ -20,35 +20,16 @@ document
     let searchQuery = document.getElementById("searchQuery").value;
     let searchType = document.getElementById("searchType").value;
 
-    const response = await searchTracks(searchQuery, searchType);
+    if (!searchQuery || !searchType) {
+      alert("Please enter a search query and select a search type.");
+      return;
+    }
 
-    console.log("Search Results:", response);
+    try {
+      const response = await searchTracks(searchQuery, searchType);
+      displaySearchResults(response);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while fetching search results.");
+    }
   });
-
-// Function to fetch user's top tracks from Spotify API
-async function getTopTracks() {
-  // Endpoint reference: https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (
-    await fetchWebApi("v1/me/top/tracks?time_range=long_term&limit=5", "GET")
-  ).items;
-}
-
-async function displayTopTracks() {
-  const topTracks = await getTopTracks();
-  console.log("Top Tracks:", topTracks);
-}
-
-const playlistId = "1r0BDMv2Y1OPswRtLxCx7I";
-const iframe = document.createElement("iframe");
-iframe.title = "Spotify Embed: Recommendation Playlist";
-iframe.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
-iframe.width = "100%";
-iframe.height = "100%";
-iframe.style.minHeight = "360px";
-iframe.frameBorder = "0";
-iframe.allow =
-  "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
-iframe.loading = "lazy";
-document.getElementById("playlistContainer").appendChild(iframe);
-
-displayTopTracks();
