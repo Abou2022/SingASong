3a4791deddb169456d533c772197202853b00bff
Bakary Sylla <syllabakary2002@gmail.com>
2024-03-25 22:40:59 -0700

    searchTracks function added

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const token = process.env.SPOTIFY_TOKEN;
 
-document
-  .getElementById("searchForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    let searchQuery = document.getElementById("searchQuery").value;
-    let searchType = document.getElementById("searchType").value;
-
-    console.log("Search Query: " + searchQuery);
-    console.log("Search Type: " + searchType);
+async function searchTracks(query) {
+  const endpoint = `v1/search?q=${encodeURIComponent(query)}&type=track`;
+  const response = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
+  return await response.json();
+}
