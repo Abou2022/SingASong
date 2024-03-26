2fcb8f0fcbfaa39461802fa2ff76fa5272e88cf9
Bakary Sylla <syllabakary2002@gmail.com>
2024-03-25 22:55:51 -0700

    script updated with get element from imput

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const token = process.env.SPOTIFY_TOKEN;
 
-async function searchTracks(query) {
-  const endpoint = `v1/search?q=${encodeURIComponent(query)}&type=track`;
-  const response = await fetch(`https://api.spotify.com/${endpoint}`, {
+async function searchTracks(query, type) {
+  const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+    query
+  )}&type=${type}`;
+  const response = await fetch(url, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   return await response.json();
 }
+
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    let searchQuery = document.getElementById("searchQuery").value;
+    let searchType = document.getElementById("searchType").value;
+
+    const response = await searchTracks(searchQuery, searchType);
+
+    console.log("Search Results:", response);
+  });
