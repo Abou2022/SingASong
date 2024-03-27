2de65bfe764d2ef1e93f39ecd1d7e400db241372
Bakary Sylla <syllabakary2002@gmail.com>
2024-03-27 14:57:01 -0700

    function handle submit form added and reponse store in data

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-const token = process.env.SPOTIFY_TOKEN;
-
-async function searchTracks(query, type) {
-  const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-    query
-  )}&type=${type}`;
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return await response.json();
-}
-
-document
-  .getElementById("searchForm")
-  .addEventListener("submit", async function (event) {
+$(document).ready(function () {
+  function handleFormSubmit(event) {
     event.preventDefault();
 
-    let searchQuery = document.getElementById("searchQuery").value;
-    let searchType = document.getElementById("searchType").value;
+    var searchQuery = $("#searchQuery").val();
+    var searchType = $("#searchType").val();
 
-    if (!searchQuery || !searchType) {
-      alert("Please enter a search query and select a search type.");
-      return;
-    }
+    const my_token =
+      "BQBo-hloo5khV2UXkAPxrWwmhTw9-ThY0BQDbO7I_ZzVkiS5o0lkB9C_0N1UUUi7EVbCc5m201UEit14Ub9Mia_6JUcEHxyUwBwa5C_2ArPIMMaonZ7WGrNHP1RPgcKCFkQZbjAcvEdkqw35O6NuM-oo8D6vTzFDuEk2RxjmfKtGaX2y6f6KbJZy5xTIMtcSk3rlzz0eRwu21gJVKtByjmXIR7LCTBKcuiWmH8mkmkS0TlrVQ8hzluWwOqXAVgzvUVZKpC5LQMt66Fevx3Z14w0s";
+
+    const url = `https://api.spotify.com/v1/search?q=${searchQuery}&type=${searchType}&limit=5`;
 
     try {
-      const response = await searchTracks(searchQuery, searchType);
-      displaySearchResults(response);
+      const response = fetch(url, {
+        headers: {
+          Authorization: `Bearer ${my_token}`,
+        },
+      });
+
+      const data = response.json();
+      displayData(data, searchType);
     } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred while fetching search results.");
+      console.error("Error fetching data:", error);
     }
-  });
+  }
+});
