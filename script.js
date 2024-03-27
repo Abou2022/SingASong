68716747cf93d2bcd445c710ab9bea7c5aba7000
Bakary Sylla <syllabakary2002@gmail.com>
2024-03-27 15:04:43 -0700

    token remove

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ $(document).ready(function () {
     var searchQuery = $("#searchQuery").val();
     var searchType = $("#searchType").val();
 
-    const my_token =
-      "BQBo-hloo5khV2UXkAPxrWwmhTw9-ThY0BQDbO7I_ZzVkiS5o0lkB9C_0N1UUUi7EVbCc5m201UEit14Ub9Mia_6JUcEHxyUwBwa5C_2ArPIMMaonZ7WGrNHP1RPgcKCFkQZbjAcvEdkqw35O6NuM-oo8D6vTzFDuEk2RxjmfKtGaX2y6f6KbJZy5xTIMtcSk3rlzz0eRwu21gJVKtByjmXIR7LCTBKcuiWmH8mkmkS0TlrVQ8hzluWwOqXAVgzvUVZKpC5LQMt66Fevx3Z14w0s";
+    const my_token = "";
 
     const url = `https://api.spotify.com/v1/search?q=${searchQuery}&type=${searchType}&limit=5`;
 
